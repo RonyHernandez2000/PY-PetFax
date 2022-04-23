d4218f9ce4628b7a206def7b9b2dfc504f53015b
Rony Hernandez <ronyhernandez3314@gmail.com>
2022-04-23 01:03:14 -0400

    finished static style

---
+++ petfax/__init__.py
@@ -0,0 +1,18 @@
+                  
+from flask import Flask
+
+# factory
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+
+    from . import pet 
+    app.register_blueprint(pet.bp)
+
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    return app
